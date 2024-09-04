2dea9ecf72ad3adb4c5e7fb3cd7989c4e8a6366b
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-09-04 10:54:55 +0200

    init database.py

--- src/database.py
+++ src/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/dbname"
+# Todo; Move to envierment variable
+SQLALCHEMY_DATABASE_URL = "postgresql://dev:dev@localhost/tudu"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
