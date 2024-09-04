fe9e78c4bf1260f8fabaf271afe5984a7292a35d
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-09-04 13:20:38 +0200

    improve the url

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Todo; Move to envierment variable
-SQLALCHEMY_DATABASE_URL = "postgresql://dev:dev@localhost/tudu"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://dev:dev@localhost/tudu"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
