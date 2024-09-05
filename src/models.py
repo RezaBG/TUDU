fc368b8ebeea7375ce1ea883a1702d7682a899a2
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-09-05 16:07:00 +0200

    refactor import and add index to Todo model

--- src/models.py
+++ src/models.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
-
-from database import Base
+from src.database import Base
 
 class Todo(Base):
     __tablename__ = "todos"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
