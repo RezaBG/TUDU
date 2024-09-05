8b51dc8fd7a555ef4a8159f1aab63d6bc20cd104
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-09-05 16:19:50 +0200

    cleanup function names and structures

--- src/crud.py
+++ src/crud.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Session
+from . import models, schemas
+from src.models import Todo
 
-from .import models, schemas
-
-def get_todos(db: Session):
+def get_todo(db: Session):
     return db.query(Todo).all()
 
 
-def create_todo(db: Session, todos: schemas.TodoCreate):
+def create_todo(db: Session, todo: schemas.TodoCreate):
     db_todo = models.Todo(title=todo.title, description=todo.description)
-    db_add(db_todo)
+    db.add(db_todo)
     db.commit()
     db.refresh(db_todo)
     return db_todo
