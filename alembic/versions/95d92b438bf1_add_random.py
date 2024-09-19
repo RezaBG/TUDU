6560d91065dd3cd3fc4cd7a66bb189263ad9ad0a
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-09-19 21:31:37 +0200

    Update env.oy and models.oy with alembin fix and random column

---
+++ alembic/versions/95d92b438bf1_add_random.py
@@ -0,0 +1,26 @@
+"""Add random
+
+Revision ID: 95d92b438bf1
+Revises: acf0995e8045
+Create Date: 2024-09-19 16:06:24.088930
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '95d92b438bf1'
+down_revision: Union[str, None] = 'acf0995e8045'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('random', sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    pass
