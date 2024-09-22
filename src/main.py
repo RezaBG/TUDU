de4100e6df7d8d72d66871d9972c047f443bf88c
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-09-22 10:13:35 +0200

    update items routes to require a user id

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
+# Dependency to get a DB session
 def get_db():
     db = SessionLocal()
     try:
@@ -17,17 +17,17 @@ def get_db():
     finally:
         db.close()
 
-# Route to get all todos
+# Rouet a get all todos for the logged-in user
 @app.get("/todos", response_model=list[schemas.TodoRead])
-async def read_todos(db: Session = Depends(get_db)):
-    todos = crud.get_todo(db)
-    return todos
+async def read_todos(db: Session = Depends(get_db), current_user: schemas.UserRead = Depends(get_current_user)):
+    return crud.get_user_todos(db=db, user_id=current_user.id)
 
 # Route to create a new todo
 @app.post("/todos", response_model=schemas.TodoRead)
-def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
-    return crud.create_todo(db=db, todo=todo)
+def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db), current_user: schemas.UserRead = Depends(get_current_user)):
+    return crud.create_todo(db=db, todo=todo, user_id=current_user.id)
 
+# Route to update an existing todo
 @app.put("/todos/{todo_id}", response_model=schemas.TodoRead)
 def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
     updated_todo = crud.update_todo(db=db, todo_id=todo_id, todo=todo)
@@ -35,6 +35,7 @@ def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(ge
         raise HTTPException(status_code= 404, detail="Todo not found - 404")
     return updated_todo
 
+# Route for delete todo
 @app.delete("/todos/{todo_id}")
 def delete_todo(todo_id: int, db: Session = Depends(get_db)):
     deleted_todo = crud.delete_todo(db=db, todo_id=todo_id)
@@ -42,14 +43,17 @@ def delete_todo(todo_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Todo not found - 404") 
     return deleted_todo
 
+# Route for create a new user
 @app.post("/register", response_model=schemas.UserRead)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
+# Route to get all users with paginaiton
 @app.get("/users/", response_model=list[schemas.UserRead])
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_users(db=db, skip=skip, limit=limit)
 
+# Route to get a single user by their ID
 @app.get("/users/{user_id}", response_model=schemas.UserRead)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     read_user = crud.get_user(db=db, user_id=user_id)
@@ -57,10 +61,12 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="user not found - 404")
     return read_user
 
+# Route to update a user
 @app.put("/users/{user_id}", response_model=schemas.UserRead)
 def update_user(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
     return crud.update_user(db=db, user_id=user_id, user=user)
 
+# Route to delete a user
 @app.delete("/users/{user_id}")
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     return crud.delete_user(db=db, user_id=user_id)
