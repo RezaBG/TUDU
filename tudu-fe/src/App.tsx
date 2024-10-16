c941d31f0e2e8399e9fb01ea1f6a1f3137b8aca4
Reza Barzegar <rezabarzegar@Rezas-MacBook-Pro.local>
2024-10-16 19:59:12 +0200

    feat: Implement todo functionality

---
+++ tudu-fe/src/App.tsx
@@ -0,0 +1,23 @@
+import TodoList from "./components/TodoList.tsx";
+
+function App() {
+    const todos = [
+        { id: 1,title: "Learn React", description: "Learn React" },
+        { id: 2, title: "Learn TypeScript", description: "Learn TS" },
+        { id: 3,title: "Build Todo App", description: "TUDU app" },
+    ];
+
+    const handleAddTodo = () => {
+        console.log("Add Todo button clicked");
+    };
+
+    return (
+        <>
+            <h1>My Todo App</h1>
+            <button onClick={handleAddTodo}>Add Todo</button>
+            <TodoList todos={todos} />
+        </>
+    );
+}
+
+export default App;
---
+++ tudu-fe/src/components/TodoItem.tsx
@@ -0,0 +1,17 @@
+interface TodoItemProps {
+    title: string;
+    description: string;
+}
+
+const TodoItem = ({ title, description} : TodoItemProps)  => {
+    return (
+        <div>
+            <h3>
+                {title}
+            </h3>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default TodoItem
