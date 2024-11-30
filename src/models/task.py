b9dd66555809cef60034cc8e5174d71f0b20bf25
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-11-30 21:36:15 +0100

    Enhance task and user models with type annotations and relationships

--- src/models/task.py
+++ src/models/task.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-
 
 from src.services.database import Base
 
@@ -8,9 +7,9 @@ from src.services.database import Base
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True, nullable=False)
-    description = Column(String, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String, index=True, nullable=False)
+    description: Mapped[str] = mapped_column(String, index=True)
     owner_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
     status: Mapped[str] = mapped_column(String, default="pending", nullable=False)
 
