e4373e7c073500963bbdd4b19a114b6b9b0d1214
Reza Barzegar Gashti <rezabarzegargashti@gmail.com>
2024-11-30 21:44:00 +0100

    Enhance user and task routers

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,4 +1,5 @@
 from .task import router as task_router
 from .user import router as user_router
+from .auth import router as auth_router
 
-__all__ = ["user_router", "task_router"]
+__all__ = ["user_router", "task_router", "auth_router"]
